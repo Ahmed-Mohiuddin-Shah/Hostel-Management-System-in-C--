1a9aea4f008f40d7bbda5b5be77d1ec255f772b5
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-18 15:29:40 +0500

    add bugCat gif to overlay

--- Hostel Managment System/overlay.h
+++ Hostel Managment System/overlay.h
@@ -3,13 +3,11 @@
 // -------------- Overlay ------------------------
 void guiOverlay() {
     using namespace variables;
-    static GUIBurgerButton testIconButton(10.0, 10.0);
-    if (testIconButton.isButtonToggled()) {
-        drawCustomText("FATAL ERROR :(", Vector2{ 10, 10 }, labels, 1, RED);
-    }
-    else {
-        drawCustomText("F SCENEZ :(", Vector2{ 10, 10 }, labels, 1, RED);
+    static GUIBurgerButton burgerButton(10, 10);
+
+    if (burgerButton.isButtonToggled()) {
+        animateBugCatGIF(burgerMenuBugCatPosition);
     }
 
-    testIconButton.draw();
+    burgerButton.draw();
 }
