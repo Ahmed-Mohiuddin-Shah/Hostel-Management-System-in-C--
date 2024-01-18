162f6bc6e22be67f31ead09e5cd0bd2770364e30
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-18 23:58:34 +0500

    add Error Popup test in fatal error screen

--- Hostel Managment System/fatalErrorScreen.h
+++ Hostel Managment System/fatalErrorScreen.h
@@ -3,14 +3,21 @@
 void fatalErrorScreen() {
 	using namespace variables;
 
+	ErrorPopup errorPopup(1000.0f, 3.0f);
+
 	while (!layerChangedHandler() && !WindowShouldClose())
 	{
 		// --------- Logic -----------------
+		errorPopup.update();
+
+		if (IsKeyPressed(KEY_SPACE)) {
+			errorPopup.showMessage("Error: We Stormin' the WHITE HOUSE with this one!!!!");
+		}
 
 		// --------- Drawing GUI -----------
 		BeginDrawing();
 		ClearBackground(H_DARK_GREY);
-
+		errorPopup.draw();
 		guiOverlay();
 		EndDrawing();
 	}
