5a5dcb54df1495ce0a182a6071e5f22150488f33
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-18 23:33:48 +0500

    add screen layer routes

--- Hostel Managment System/loadingScreen.h
+++ Hostel Managment System/loadingScreen.h
@@ -2,7 +2,7 @@
 
 variables::FileLoadStatus loadHostelDB() {
 
-	// std::this_thread::sleep_for(std::chrono::seconds(5));  // HEHE Delay for Funsies :)
+	std::this_thread::sleep_for(std::chrono::seconds(3));  // HEHE Delay for Funsies :)
 
 	using namespace variables;
 	if (!FileReader::checkIfFileExists(ProjectInfo::fileName)) {
@@ -62,15 +62,15 @@ void loadingScreen() {
 		drawCustomText("Hostel File Does Not Exist :(", Vector2{10, 10}, labels, 1, H_WHITE);
 		EndDrawing();
 
-		// std::this_thread::sleep_for(std::chrono::seconds(2));
+		std::this_thread::sleep_for(std::chrono::seconds(2));
 
 		currentLayer = CREATE_NEW_HOSTEL_SCREEN;
-
-		currentLayer = FATAL_ERROR_SCREEN;
 		break;
 	case variables::LOADED_SUCCESSFULLY:
+		currentLayer = FATAL_ERROR_SCREEN;
 		break;
 	case variables::FAILED_TO_LOAD:
+		currentLayer = FATAL_ERROR_SCREEN;
 		break;
 	default:
 		currentLayer = FATAL_ERROR_SCREEN;
