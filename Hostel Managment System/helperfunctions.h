7a18949966c9d20c5dd7bd295b7423abbb06c437
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-20 01:59:21 +0500

    add countNewLineCharacters function

--- Hostel Managment System/helperfunctions.h
+++ Hostel Managment System/helperfunctions.h
@@ -66,3 +66,12 @@ bool writeHostelInfoToFile() {
     }
 
 }
+
+int countNewLineCharacters(std::string s) {
+    int count = 0;
+
+    for (int i = 0; i < s.size(); i++)
+        if (s[i] == '\n') count++;
+
+    return count;
+}
