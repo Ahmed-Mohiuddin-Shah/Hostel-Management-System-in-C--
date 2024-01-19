5ce1ac432c095e18b7addc59478ca30b9a363d0a
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-19 20:10:05 +0500

    add display invoices screen

---
+++ Hostel Managment System/displayInvoicesScreen.h
@@ -0,0 +1,25 @@
+#pragma once
+
+void displayInvoicesScreen() {
+	using namespace variables;
+
+	ErrorPopup errorPopup(1500.0f, 3.0f);
+
+	while (!layerChangedHandler())
+	{
+		// --------- Logic -----------------
+		errorPopup.update();
+
+		// --------- Drawing GUI -----------
+		BeginDrawing();
+		ClearBackground(H_DARK_GREY);
+		errorPopup.draw();
+
+		drawCustomBodyText("Student DEETS:", Vector2{ 0, 0 }, headings, 1, H_DARK_BLUE);
+
+		drawCustomBodyText(hostelInstance.getAllInvoicesTable().c_str(), Vector2{ 0, 115 }, labels, 1, H_WHITE);
+
+		guiOverlay();
+		EndDrawing();
+	}
+}
