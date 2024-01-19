976965ff48eb29758c9bcc0ebb69fb6551ff8360
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-19 22:25:30 +0500

    implement addRoomScreen

--- Hostel Managment System/addRoomScreen.h
+++ Hostel Managment System/addRoomScreen.h
@@ -5,12 +5,39 @@ void addRoomScreen() {
 
 	ErrorPopup errorPopup(1500.0f, 3.0f);
 
-
+	TextInputBox roomNumberInputBox = { 90, 140, "Room Number", true, 5 };
+	GUIButton addRoomButton = { 90, 190, "Add Room" };
 
 	while (!layerChangedHandler())
 	{
 		// --------- Logic -----------------
 		errorPopup.update();
+		roomNumberInputBox.update();
+
+		if (addRoomButton.isClicked()) {
+			
+			int roomNumber;
+
+			try
+			{
+				roomNumber = std::stoi(roomNumberInputBox.getInputText());
+			}
+			catch (const std::exception e)
+			{
+				fatalErrorMessage = e.what();
+				currentLayer = FATAL_ERROR_SCREEN;
+			}
+
+			if (hostelInstance.roomNumberExists(roomNumber)) {
+				errorPopup.showMessage("Room Already Exists!!!\n     Try A different Number!!");
+			}
+			else {
+				hostelInstance.addRoom(roomNumber);
+				successPopupMessage = "Room Added Successfully!!";
+				globalShouldShowSuccessPopup = true;
+				currentLayer = ROOM_DETAILS_SCREEN;
+			}
+		}
 
 		// --------- Drawing GUI -----------
 		BeginDrawing();
@@ -18,6 +45,9 @@ void addRoomScreen() {
 		errorPopup.draw();
 
 		drawCustomBodyText("Add Room:", Vector2{ 0, 0 }, headings, 1, H_DARK_BLUE);
+		roomNumberInputBox.draw();
+		addRoomButton.draw();
+
 
 		guiOverlay();
 		EndDrawing();
