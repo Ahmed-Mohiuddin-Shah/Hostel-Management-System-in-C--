d8bc822bfad5a1b78ab2db6a98facde65e2b3077
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-19 20:10:53 +0500

    add room details screen

---
+++ Hostel Managment System/roomDetailsScreen.h
@@ -0,0 +1,25 @@
+#pragma once
+
+void roomDetailsScreen() {
+	using namespace variables;
+
+	ErrorPopup errorPopup(1500.0f, 3.0f);
+
+	while (!layerChangedHandler())
+	{
+		// --------- Logic -----------------
+		errorPopup.update();
+
+		// --------- Drawing GUI -----------
+		BeginDrawing();
+		ClearBackground(H_DARK_GREY);
+		errorPopup.draw();
+
+		drawCustomBodyText("Student DEETS:", Vector2{ 0, 0 }, headings, 1, H_DARK_BLUE);
+
+		drawCustomBodyText(hostelInstance.getAllRoomsTable().c_str(), Vector2{ 0, 115 }, labels, 1, H_WHITE);
+
+		guiOverlay();
+		EndDrawing();
+	}
+}
