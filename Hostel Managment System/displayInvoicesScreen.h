2fc2546c48cc2d78b1b969cfe0c29ad3af5a75eb
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-20 02:06:05 +0500

    implement scrollView in all the details screens

--- Hostel Managment System/displayInvoicesScreen.h
+++ Hostel Managment System/displayInvoicesScreen.h
@@ -5,19 +5,22 @@ void displayInvoicesScreen() {
 
 	ErrorPopup errorPopup(1500.0f, 3.0f);
 
+	GUIScrollView scrollView = { 90, 140, hostelInstance.getAllInvoicesTable() };
+
 	while (!layerChangedHandler())
 	{
 		// --------- Logic -----------------
 		errorPopup.update();
+		scrollView.update();
 
 		// --------- Drawing GUI -----------
 		BeginDrawing();
 		ClearBackground(H_DARK_GREY);
 
+		scrollView.draw();
 
+		DrawRectangle(90, 0, screenWidth, 140, H_DARK_GREY);
 		drawCustomBodyText("Invoices DEETS:", Vector2{ 0, 0 }, headings, 1, H_DARK_BLUE);
-
-		drawCustomBodyText(hostelInstance.getAllInvoicesTable().c_str(), Vector2{ 0, 115 }, labels, 1, H_WHITE);
 
 		errorPopup.draw();
 		guiOverlay();
