4b0013a29e5989adbbc6cff2c42094e92b19811e
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-19 14:00:37 +0500

    changed details to DEETS

--- Hostel Managment System/hostelDetailsScreen.h
+++ Hostel Managment System/hostelDetailsScreen.h
@@ -15,7 +15,7 @@ void hostelDetailsScreen() {
 		// --------- Drawing GUI -----------
 		BeginDrawing();
 		ClearBackground(H_DARK_GREY);
-		drawCustomBodyText("Hostel Details:", Vector2{ 0, 0 }, variables::headings, 1, WHITE);
+		drawCustomBodyText("Hostel DEETS:", Vector2{ 0, 0 }, variables::headings, 1, WHITE);
 		drawCustomBodyText(TextFormat("Hostel Name          : %s", hostelName.c_str()), Vector2{0, 140}, variables::labels, 1, variables::H_BLUE);
 		drawCustomBodyText(TextFormat("Hostel Address       : %s", hostelAddress.c_str()), Vector2{ 0, 170 }, variables::labels, 1, variables::H_BLUE);
 		drawCustomBodyText(TextFormat("Hostel Phone Number  : %s", hostelPhNo.c_str()), Vector2{ 0, 200 }, variables::labels, 1, variables::H_BLUE);
