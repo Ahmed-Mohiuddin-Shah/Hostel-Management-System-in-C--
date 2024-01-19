4e8a9534f1a71481a9f9d5c52ad457cec460982b
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-19 23:02:05 +0500

    implement add staff screen

--- Hostel Managment System/addStaffScreen.h
+++ Hostel Managment System/addStaffScreen.h
@@ -1,55 +1,128 @@
 #pragma once
 
+namespace addStaffFuncs {
+	std::string validateInputs(TextInputBox* inputBoxes) {
+
+		if (inputBoxes[0].getInputText().empty()) {
+			return "Error 404: ID not found!\n     Please provide a valid staff ID.";
+		}
+
+		if (inputBoxes[1].getInputText().empty()) {
+			return "Error: Staff needs a name!\n     Did you hire a ghost?";
+		}
+
+		if (inputBoxes[2].getInputText().empty()) {
+			return "Uh-oh! Looks like our staff is too shy to reveal their CNIC.\n     Please share it with us!";
+		}
+
+		if (inputBoxes[3].getInputText().empty()) {
+			return "Error: Missing contact info! Our staff might be avoiding telemarketers.\n     Help us reach them!";
+		}
+
+		if (inputBoxes[4].getInputText().empty()) {
+			return "Error: Staff without a role is like a ship without\n     a captain. Specify a role, please!";
+		}
+
+		return "valid";
+	}
+}
+
 void addStaffScreen() {
+
 	using namespace variables;
+	GUIButton addStaffButton(90, 390, "Add Staff");
 
-	ErrorPopup errorPopup(1500.0f, 3.0f);
+	const size_t numInputBoxes = 5;
+	TextInputBox inputBoxes[numInputBoxes] = {
+		{90, 140, "ID", true, 11},
+		{90, 190, "Name", false, 64},
+		{90, 240, "CNIC", true, 13},
+		{90, 290, "Phone Number", true, 11},
+		{90, 340, "Role", false, 30},
+	};
 
-	TextInputBox roomNumberInputBox = { 90, 140, "Room Number", true, 5 };
-	GUIButton addRoomButton = { 90, 190, "Add Room" };
+	bool anySelected = false;
 
-	while (!layerChangedHandler())
+	ErrorPopup errorPopup(1500.0f, 2.0f);
+
+	while (!layerChangedHandler() && !shouldExit)
 	{
 		// --------- Logic -----------------
-		errorPopup.update();
-		roomNumberInputBox.update();
+		// Update input boxes
+		for (size_t i = 0; i < numInputBoxes; ++i) {
+			inputBoxes[i].update();
+		}
 
-		if (addRoomButton.isClicked()) {
-
-			int roomNumber;
-
-			try
-			{
-				roomNumber = std::stoi(roomNumberInputBox.getInputText());
-			}
-			catch (const std::exception e)
-			{
-				fatalErrorMessage = e.what();
-				currentLayer = FATAL_ERROR_SCREEN;
-			}
-
-			if (hostelInstance.roomNumberExists(roomNumber)) {
-				errorPopup.showMessage("Room Already Exists!!!\n     Try A different Number!!");
-			}
-			else {
-				hostelInstance.addRoom(roomNumber);
-				successPopupMessage = "Room Added Successfully!!";
-				globalShouldShowSuccessPopup = true;
-				currentLayer = ROOM_DETAILS_SCREEN;
+		// Deselect other input boxes when one is clicked
+		for (size_t i = 0; i < numInputBoxes; ++i) {
+			if (inputBoxes[i].isActive()) {
+				anySelected = true;
+				break;
 			}
 		}
+
+		if (anySelected) {
+			for (size_t i = 0; i < numInputBoxes; ++i) {
+				if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(GetMousePosition(), inputBoxes[i].getBounds())) {
+					// If a new input box is clicked, deactivate others
+					for (size_t j = 0; j < numInputBoxes; ++j) {
+						if (i != j) {
+							inputBoxes[j].deSelect();
+						}
+					}
+					break;
+				}
+			}
+		}
+
+		errorPopup.update();
 
 		// --------- Drawing GUI -----------
 		BeginDrawing();
 		ClearBackground(H_DARK_GREY);
+		drawCustomText("Add Staff:", Vector2{ 90, 10 }, headings, 1, H_WHITE);
+		addStaffButton.draw();
+
+		// Draw the input boxes
+		for (size_t i = 0; i < numInputBoxes; ++i) {
+			inputBoxes[i].draw();
+		}
+
+		if (addStaffButton.isClicked()) {
+
+			std::string validityMessage = addStaffFuncs::validateInputs(inputBoxes);
+			if (validityMessage == "valid") {
+
+				int staffID;
+
+				try
+				{
+					staffID = std::stoi(inputBoxes[0].getInputText());
+				}
+				catch (const std::exception e)
+				{
+					fatalErrorMessage = e.what();
+					currentLayer = FATAL_ERROR_SCREEN;
+				}
+
+				if (hostelInstance.checkIfStaffExists(staffID)) {
+					errorPopup.showMessage("Staff ID Already Exists!!!\n     Try A different ID!!");
+				}
+				else {
+					hostelInstance.addStaff(staffID, inputBoxes[1].getInputText(), inputBoxes[2].getInputText(), inputBoxes[3].getInputText(), inputBoxes[4].getInputText());
+					successPopupMessage = "Staff Added Successfully!!";
+					globalShouldShowSuccessPopup = true;
+					currentLayer = STAFF_DETAILS_SCREEN;
+				}
+			}
+			else {
+				errorPopup.showMessage(validityMessage);
+			}
+		}
+
 		errorPopup.draw();
-
-		drawCustomBodyText("Add Room:", Vector2{ 0, 0 }, headings, 1, H_DARK_BLUE);
-		roomNumberInputBox.draw();
-		addRoomButton.draw();
-
-
 		guiOverlay();
 		EndDrawing();
 	}
+
 }
