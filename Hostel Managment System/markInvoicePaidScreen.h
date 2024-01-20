bfaee89794366a782b34a49044932194e12ea6ae
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-20 09:56:27 +0500

    implement markInvoicePaidScreen

---
+++ Hostel Managment System/markInvoicePaidScreen.h
@@ -0,0 +1,60 @@
+#pragma once
+
+void markInvoicePaidScreen() {
+	using namespace variables;
+
+	ErrorPopup errorPopup(1500.0f, 3.0f);
+
+	TextInputBox invoiceIDInputBox = { 90, 150, "Invoice ID", true, 10 };
+	GUIButton updateInvoiceStatusButton = { 90, 200, "Update Invoice Status" };
+
+	while (!layerChangedHandler())
+	{
+		// --------- Logic -----------------
+		errorPopup.update();
+		invoiceIDInputBox.update();
+
+		if (updateInvoiceStatusButton.isClicked()) {
+			if (invoiceIDInputBox.getInputText().empty()) {
+				errorPopup.showMessage("How can we update Status without an ID!\n     Enter an Invoice ID!!");
+			}
+			else {
+
+				int invoiceID;
+
+				try
+				{
+					invoiceID = std::stoi(invoiceIDInputBox.getInputText());
+				}
+				catch (const std::exception e)
+				{
+					fatalErrorMessage = e.what();
+					currentLayer = FATAL_ERROR_SCREEN;
+				}
+
+				if (!hostelInstance.checkIfInvoiceExists(invoiceID)) {
+					errorPopup.showMessage("Invoice Does not Exist!!!\n     Try A different ID!!");
+				}
+				else {
+					hostelInstance.updateInvoiceStatus(invoiceID, true);
+					successPopupMessage = "Invoice Updated Successfully!!";
+					globalShouldShowSuccessPopup = true;
+					currentLayer = DISPLAY_INVOICES_SCREEN;
+				}
+			}
+		}
+
+		// --------- Drawing GUI -----------
+		BeginDrawing();
+		ClearBackground(H_DARK_GREY);
+
+
+		drawCustomBodyText("Update Invoice:", Vector2{ 0, 0 }, headings, 1, H_DARK_BLUE);
+		invoiceIDInputBox.draw();
+		updateInvoiceStatusButton.draw();
+
+		errorPopup.draw();
+		guiOverlay();
+		EndDrawing();
+	}
+}
